bf75992e11e8305e49335ea0c00c236ebb301f11
Gustaf franzèn <gustaffranzen@icloud.com>
2022-12-15 15:19:27 +0100

    fixed minor bug with args

--- src/main.c
+++ src/main.c
@@ -20,12 +20,13 @@ void usage(const char *prog)
 	printf("Usage:\n");
 	printf("%s <filename>\n", prog);
 	printf("Options:\n");
-	printf("	<\033[1;33m-o\033[0m> <filename>	specify output file\n");
-	printf("	<\033[1;33m-v\033[0m> <\033[1;33m-vv\033[0m>	verbosity level, useful for debuging\n");
-	printf("	<\033[1;33m-h\033[0m>		display usage info\n");
-	printf("	<\033[1;33m-f\033[0m> <(x, hex) | (b, bin)>	specify output format\n");
+	printf("	<\033[1;33m-o\033[0m> <filename>		specify output file\n");
+	printf("	<\033[1;33m-v\033[0m> <\033[1;33m-vv\033[0m>		verbosity level, useful for debuging\n");
+	printf("	<\033[1;33m-h\033[0m>			display usage info\n");
+	printf("	<\033[1;33m-f\033[0m> <x|b|hex|bin>	specify output format\n");
 	printf("Example: \n");
 	printf("	%s main.S -o myprogram.bin\n", prog);
+	exit(-1);
 }
 void generate_binary(const char * src, const char * dst, i32_t verbosity, i32_t hex)
 {
@@ -45,7 +46,8 @@ i32_t main(i32_t argc, char *argv[])
 		fprintf(stderr, "no input file specified, try -h for help");
 		exit(-1);
 	}
-
+	if(!strcmp(argv[1], "-h"))
+		usage(argv[0]);
 	input = argv[1];
 
 	while((opt = getopt(argc-1, argv+1, ":o:f:vh")) != -1) {
